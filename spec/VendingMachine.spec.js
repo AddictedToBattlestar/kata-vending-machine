c22221fff457981d42cfff4ba5244d41c917e3f1
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-05-09 21:25:16 -0400

    Completed the refactor so that the vendingMachine and the coinService don't actually interact in their tests.

--- spec/VendingMachine.spec.js
+++ spec/VendingMachine.spec.js
@@ -1,13 +1,28 @@
 describe('Vending machine', () => {
   let subject;
+  let mockCoinService;
 
   when('started', () => {
     beforeEach(() => {
+      mockCoinService = jasmine.createSpyObj('coinService', ['insertCoin', 'getAmountInserted']);
+      spyOn(nenaner.coinService, 'create').and.returnValue(mockCoinService);
+      mockCoinService.getAmountInserted.and.returnValue(0);
+
       subject = vendingMachine();
     });
 
     it('displays the message "INSERT COIN"', () => {
       expect(subject.getDisplay()).toEqual('INSERT COIN');
+    });
+
+    when('coins are inserted', () => {
+      beforeEach(() => {
+        mockCoinService.getAmountInserted.and.returnValue(1.5);
+      });
+
+      it('displays the amount with a dollar sign and 2 decimal places', () => {
+        expect(subject.getDisplay()).toEqual('$1.50');
+      });
     });
   });
 });

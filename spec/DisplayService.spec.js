fec007624a3a98dd751083418745a993967e6a1d
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-05-10 23:11:44 -0400

    Copied the display centric tests for the Vending Machine over to the Display service.  Time to cut over.

---
+++ spec/DisplayService.spec.js
@@ -0,0 +1,45 @@
+describe('Display service', () => {
+  let subject;
+  let mockGetAmountInserted;
+
+  beforeEach(() => {
+    mockGetAmountInserted = jasmine.createSpy('GetAmountInserted');
+    mockGetAmountInserted.and.returnValue(0);
+
+    subject = nenaner.displayService.create(mockGetAmountInserted);
+  });
+
+  it('displays the message "INSERT COIN"', () => {
+    expect(subject.getDisplay()).toEqual('INSERT COIN');
+  });
+
+  when('coins are inserted', () => {
+    beforeEach(() => {
+      mockGetAmountInserted.and.returnValue(1.5);
+    });
+
+    it('displays the amount with a dollar sign and 2 decimal places', () => {
+      expect(subject.getDisplay()).toEqual('$1.50');
+    });
+  });
+
+  when('a temporary message is asked to be displayed', () => {
+    beforeEach(() => {
+      subject.setTemporaryMessage('fake-temporary-message');
+    });
+
+    it('displays the temporary message', () => {
+      expect(subject.getDisplay()).toEqual('fake-temporary-message');
+    });
+
+    and('when checked a second time', () => {
+      beforeEach(() => {
+        subject.getDisplay();
+      });
+
+      it('displays the message "INSERT COIN"', () => {
+        expect(subject.getDisplay()).toEqual('INSERT COIN');
+      });
+    });
+  });
+});

8f316162957144ec2612e73b45589df363b96ae3
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-05-12 14:45:01 -0400

    Now the tests the specfile for the vendingMachine object are again unit tests.

--- spec/VendingMachine.spec.js
+++ spec/VendingMachine.spec.js
@@ -2,6 +2,7 @@ describe('Vending machine', () => {
   let subject;
   let mockCoinService;
   let mockDisplayService;
+  let mockProductService;
 
   when('started', () => {
     beforeEach(() => {
@@ -12,11 +13,10 @@ describe('Vending machine', () => {
       setupMocksForTheDisplayService();
       mockDisplayService.getDisplay.and.returnValue('fake-display-message');
 
-      subject = nenaner.vendingMachine();
-    });
+      setupMocksForTheProductService();
+      mockProductService.getDispenser.and.returnValue('fake-dispenser-item');
 
-    it('has an empty dispenser', () => {
-      expect(subject.getDispenser()).not.toBeDefined();
+      subject = nenaner.vendingMachine();
     });
 
     it('indirectly provides from the coinService coins that have been returned', () => {
@@ -27,82 +27,27 @@ describe('Vending machine', () => {
       expect(subject.getDisplay()).toEqual('fake-display-message');
     });
 
-    describe('select product testing', () => {
-      when('the button for Cola is pressed', () => {
-        beforeEach(() => {
-          subject.colaButtonPressed();
-        });
+    it('has setup the dispenser to feed from the productService', () => {
+      expect(subject.getDispenser()).toEqual('fake-dispenser-item');
+    });
 
-        it('displays the temporary message "PRICE $1.00"', () => {
-          expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('PRICE $1.00');
-        });
+    when('a Cola is selected', () => {
+      beforeEach(() => {
+        subject.colaButtonPressed();
       });
 
-      when('the button for chips is pressed', () => {
-        beforeEach(() => {
-          subject.chipsButtonPressed();
-        });
+      it('makes a call to the productService for a Cola', () => {
+        expect(mockProductService.productPurchaseRequest).toHaveBeenCalledWith('Cola');
+      });
+    });
 
-        it('displays the temporary message "PRICE $0.50"', () => {
-          expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('PRICE $0.50');
-        });
+    when('a Chips are selected', () => {
+      beforeEach(() => {
+        subject.chipsButtonPressed();
       });
 
-      when('the button for Cola is pressed and there are enough coins inserted', () => {
-        beforeEach(() => {
-          mockCoinService.getAmountInserted.and.returnValue(1);
-          subject.colaButtonPressed();
-        });
-
-        it('displays the temporary message "THANK YOU"', () => {
-          expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
-        });
-
-        it('dispenses a Cola', () => {
-          expect(subject.getDispenser()).toEqual('Cola');
-        });
-
-        it('has reduced the amount inserted for the cost of the Cola', () => {
-          expect(mockCoinService.processPurchase).toHaveBeenCalledWith(1);
-        });
-      });
-
-      when('the button for Chips is pressed and there are enough coins inserted', () => {
-        beforeEach(() => {
-          mockCoinService.getAmountInserted.and.returnValue(1);
-          subject.chipsButtonPressed();
-        });
-
-        it('displays the temporary message "THANK YOU"', () => {
-          expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
-        });
-
-        it('dispenses a Chips', () => {
-          expect(subject.getDispenser()).toEqual('Chips');
-        });
-
-        it('has reduced the amount inserted for the cost of the Cola', () => {
-          expect(mockCoinService.processPurchase).toHaveBeenCalledWith(0.5);
-        });
-      });
-
-      when('the button for Cola is pressed and there are too many coins inserted', () => {
-        beforeEach(() => {
-          mockCoinService.getAmountInserted.and.returnValue(1.25);
-          subject.colaButtonPressed();
-        });
-
-        it('displays the temporary message "THANK YOU"', () => {
-          expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
-        });
-
-        it('dispenses a Cola', () => {
-          expect(subject.getDispenser()).toEqual('Cola');
-        });
-
-        it('has reduced the amount inserted for the cost of the Cola', () => {
-          expect(mockCoinService.processPurchase).toHaveBeenCalledWith(1);
-        });
+      it('makes a call to the productService for Chips', () => {
+        expect(mockProductService.productPurchaseRequest).toHaveBeenCalledWith('Chips');
       });
     });
   });
@@ -123,5 +68,13 @@ describe('Vending machine', () => {
       'setTemporaryMessage'
     ]);
     spyOn(nenaner.displayService, 'create').and.returnValue(mockDisplayService);
+  }
+
+  function setupMocksForTheProductService() {
+    mockProductService = jasmine.createSpyObj('productService', [
+      'productPurchaseRequest',
+      'getDispenser'
+    ]);
+    spyOn(nenaner.productService, 'create').and.returnValue(mockProductService);
   }
 });

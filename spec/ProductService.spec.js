088a770de1d85deb6a468b956676c47f327f9242
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-05-12 15:31:57 -0400

    I forgot a test to ensure on start that the dispenser is empty.

--- spec/ProductService.spec.js
+++ spec/ProductService.spec.js
@@ -3,87 +3,93 @@ describe('Product service', () => {
   let mockCoinService;
   let mockDisplayService;
 
-  beforeEach(() => {
-    setupMocksForTheCoinService();
-    setupMocksForTheDisplayService();
-
-    subject = nenaner.productService.create(mockCoinService, mockDisplayService);
-  });
-
-  when('the button for Cola is pressed', () => {
+  when('created', () => {
     beforeEach(() => {
-      subject.productPurchaseRequest('Cola');
+      setupMocksForTheCoinService();
+      setupMocksForTheDisplayService();
+
+      subject = nenaner.productService.create(mockCoinService, mockDisplayService);
     });
 
-    it('displays the temporary message "PRICE $1.00"', () => {
-      expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('PRICE $1.00');
-    });
-  });
-
-  when('the button for chips is pressed', () => {
-    beforeEach(() => {
-      subject.productPurchaseRequest('Chips');
+    it('has an empty dispenser', () => {
+      expect(subject.getDispenser()).not.toBeDefined();
     });
 
-    it('displays the temporary message "PRICE $0.50"', () => {
-      expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('PRICE $0.50');
-    });
-  });
+    when('the button for Cola is pressed', () => {
+      beforeEach(() => {
+        subject.productPurchaseRequest('Cola');
+      });
 
-  when('the button for Cola is pressed and there are enough coins inserted', () => {
-    beforeEach(() => {
-      mockCoinService.getAmountInserted.and.returnValue(1);
-      subject.productPurchaseRequest('Cola');
-    });
-
-    it('displays the temporary message "THANK YOU"', () => {
-      expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
+      it('displays the temporary message "PRICE $1.00"', () => {
+        expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('PRICE $1.00');
+      });
     });
 
-    it('dispenses a Cola', () => {
-      expect(subject.getDispenser()).toEqual('Cola');
+    when('the button for chips is pressed', () => {
+      beforeEach(() => {
+        subject.productPurchaseRequest('Chips');
+      });
+
+      it('displays the temporary message "PRICE $0.50"', () => {
+        expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('PRICE $0.50');
+      });
     });
 
-    it('has reduced the amount inserted for the cost of the Cola', () => {
-      expect(mockCoinService.processPurchase).toHaveBeenCalledWith(1);
-    });
-  });
+    when('the button for Cola is pressed and there are enough coins inserted', () => {
+      beforeEach(() => {
+        mockCoinService.getAmountInserted.and.returnValue(1);
+        subject.productPurchaseRequest('Cola');
+      });
 
-  when('the button for Chips is pressed and there are enough coins inserted', () => {
-    beforeEach(() => {
-      mockCoinService.getAmountInserted.and.returnValue(1);
-      subject.productPurchaseRequest('Chips');
-    });
+      it('displays the temporary message "THANK YOU"', () => {
+        expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
+      });
 
-    it('displays the temporary message "THANK YOU"', () => {
-      expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
-    });
+      it('dispenses a Cola', () => {
+        expect(subject.getDispenser()).toEqual('Cola');
+      });
 
-    it('dispenses a Chips', () => {
-      expect(subject.getDispenser()).toEqual('Chips');
-    });
-
-    it('has reduced the amount inserted for the cost of the Cola', () => {
-      expect(mockCoinService.processPurchase).toHaveBeenCalledWith(0.5);
-    });
-  });
-
-  when('the button for Cola is pressed and there are too many coins inserted', () => {
-    beforeEach(() => {
-      mockCoinService.getAmountInserted.and.returnValue(1.25);
-      subject.productPurchaseRequest('Cola');
+      it('has reduced the amount inserted for the cost of the Cola', () => {
+        expect(mockCoinService.processPurchase).toHaveBeenCalledWith(1);
+      });
     });
 
-    it('displays the temporary message "THANK YOU"', () => {
-      expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
+    when('the button for Chips is pressed and there are enough coins inserted', () => {
+      beforeEach(() => {
+        mockCoinService.getAmountInserted.and.returnValue(1);
+        subject.productPurchaseRequest('Chips');
+      });
+
+      it('displays the temporary message "THANK YOU"', () => {
+        expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
+      });
+
+      it('dispenses a Chips', () => {
+        expect(subject.getDispenser()).toEqual('Chips');
+      });
+
+      it('has reduced the amount inserted for the cost of the Cola', () => {
+        expect(mockCoinService.processPurchase).toHaveBeenCalledWith(0.5);
+      });
     });
 
-    it('dispenses a Cola', () => {
-      expect(subject.getDispenser()).toEqual('Cola');
-    });
+    when('the button for Cola is pressed and there are too many coins inserted', () => {
+      beforeEach(() => {
+        mockCoinService.getAmountInserted.and.returnValue(1.25);
+        subject.productPurchaseRequest('Cola');
+      });
 
-    it('has reduced the amount inserted for the cost of the Cola', () => {
-      expect(mockCoinService.processPurchase).toHaveBeenCalledWith(1);
+      it('displays the temporary message "THANK YOU"', () => {
+        expect(mockDisplayService.setTemporaryMessage).toHaveBeenCalledWith('THANK YOU');
+      });
+
+      it('dispenses a Cola', () => {
+        expect(subject.getDispenser()).toEqual('Cola');
+      });
+
+      it('has reduced the amount inserted for the cost of the Cola', () => {
+        expect(mockCoinService.processPurchase).toHaveBeenCalledWith(1);
+      });
     });
   });
 
